038dd92c8816468751424d875af004bb1adfff9d
Christophe Larsonneur <clarsonneur@gmail.com>
2016-08-11 23:25:59 +0200

    Remove unused cli parameters

--- upstream/github/cli.go
+++ upstream/github/cli.go
@@ -13,14 +13,12 @@ type githubApp struct {
     App *kingpin.Application
     params Params
     socket string
-    src_path string
     Yaml goforjj.YamlPlugin
 }
 
 type Params struct {
     socket_file *string
     socket_path *string
-    src_path *string
     daemon *bool // Currently not used - Lot of concerns with daemonize in go... Stay in foreground
 }
 
@@ -35,7 +33,6 @@ func (a *githubApp)init() {
     daemon.Command("start", "start github REST API service")
     a.params.socket_file = daemon.Flag("socket-file", "Socket file to use").Default(a.Yaml.Runtime.Service.Socket).String()
     a.params.socket_path = daemon.Flag("socket-path", "Socket file path to use").Default("/tmp/forjj-socks").String()
-    a.params.src_path = daemon.Flag("source-path", "Path to github plugin source file").Default("/src").String()
     a.params.daemon = daemon.Flag("daemon", "Start process in background like a daemon").Short('d').Bool()
 }
 
