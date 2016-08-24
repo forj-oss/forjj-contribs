3d1ac324e9acd4ea4192577854320c6adee356cb
Christophe Larsonneur <clarsonneur@gmail.com>
2016-08-24 18:26:24 +0200

    Fix templates.yaml existence error.

--- ci/jenkins/create.go
+++ ci/jenkins/create.go
@@ -27,7 +27,7 @@ func (r *CreateReq) check_source_existence(ret *goforjj.PluginData) (p *JenkinsP
 
     p.template_dir = *cliApp.params.template_dir
     templatef := path.Join(p.template_dir, template_file)
-    if _, err := os.Stat(templatef) ; err == nil {
+    if _, err := os.Stat(templatef) ; err != nil {
         log.Printf(ret.Errorf("Unable to find templates definition file '%s'. %s.", templatef, err))
         return
     }
