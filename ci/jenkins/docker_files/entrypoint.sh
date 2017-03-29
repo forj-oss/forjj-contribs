a84320d09ddc427e1d3cf4d0ff3c04189ff02b5a
Chris <clarsonneur@gmail.com>
2017-03-29 17:01:19 +0200

    Test if UID/GID has been set in container run.

--- ci/jenkins/docker_files/entrypoint.sh
+++ ci/jenkins/docker_files/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -x
 
+if [ "$UID" = "" ] || [ "$GID" = "" ]
+then
+   echo "Missing UID/GID env variable"
+   exit 1
+fi
+
 /bin/update_user.sh $UID $GID
 exec /bin/su devops -c "/bin/jenkins $*"
