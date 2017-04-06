4121e86fabee59d34fb1df7f5b462073ec3bfa28
Christophe Larsonneur <clarsonneur@gmail.com>
2017-04-06 23:14:39 +0200

    Define group contents and maintain users

--- upstream/github/github_plugin.go
+++ upstream/github/github_plugin.go
@@ -24,7 +24,12 @@ type GitHubSourceStruct struct {
     Organization string                  // Organization name
     OrgDisplayName string                // Organization's display name.
     Users map[string]string              // Collection of users role at organization level
-    Groups map[string]string             // Collection of groups role at organization level
+    Groups map[string]TeamStruct         // Collection of Team role at organization level
+}
+
+type TeamStruct struct {
+	Role string       // Default role to apply at organization level for new Repositories
+	Users []string    // list of users
 }
 
 const github_source_file = "github.yaml"
