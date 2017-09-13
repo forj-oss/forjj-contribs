8b184c01bf5d895e12265453987f23e17e3a25bc
Christophe Larsonneur <clarsonneur@gmail.com>
2017-09-13 23:34:36 +0200

    Remove debug mode in start.sh

--- ci/jenkins/templates/bin/start.sh
+++ ci/jenkins/templates/bin/start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 #
 #
 
@@ -90,6 +90,7 @@ fi
 if [[ "$ADMIN_PWD" != "" ]]
 then
    ADMIN="-e SIMPLE_ADMIN_PWD=$ADMIN_PWD"
+   echo "Admin password set."
 fi
 
 {{ if .Deploy.Ssl.Certificate }}\
@@ -99,6 +100,7 @@ then
    exit 1
 fi
 echo "$CERTIFICATE_KEY" > .certificate.key
+echo "Certificate set."
 
 JENKINS_OPTS='JENKINS_OPTS=--httpPort=-1 --httpsPort=8443 --httpsCertificate=/tmp/certificate.crt --httpsPrivateKey=/tmp/certificate.key'
 JENKINS_MOUNT="-v ${SRC}certificate.crt:/tmp/certificate.crt -v ${SRC}.certificate.key:/tmp/certificate.key"
@@ -106,7 +108,7 @@ JENKINS_MOUNT="-v ${SRC}certificate.crt:/tmp/certificate.crt -v ${SRC}.certifica
 sudo docker run --restart always -d -p $SERVICE_PORT:8443 -e "$JENKINS_OPTS" $JENKINS_MOUNT --name {{ .JenkinsImage.Name }}-dood $ADMIN $CREDS $PROXY $DOCKER_OPTS $TAG_NAME
 
 {{ else }}
-sudo docker run --restart always -d -p $SERVICE_PORT:8080 --name {{ .JenkinsImage.Name }}-dood $CREDS $PROXY $DOCKER_OPTS $TAG_NAME
+sudo docker run --restart always -d -p $SERVICE_PORT:8080 --name {{ .JenkinsImage.Name }}-dood $ADMIN $CREDS $PROXY $DOCKER_OPTS $TAG_NAME
 {{ end }}\
 
 if [ $? -ne 0 ]
