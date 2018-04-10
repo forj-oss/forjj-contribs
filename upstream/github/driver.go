50a7d81542f8830bb98c534ed54e4082d805d8fc
Christophe Larsonneur <clarsonneur@gmail.com>
2018-04-10 15:29:37 +0200

    Move github to his own repository

--- upstream/github/driver.go
+++
@@ -1,4 +0,0 @@
-package main
-
-//go:generate go build -o $GOPATH/bin/forjj-genapp forjj-contribs/upstream/github/vendor/github.com/forj-oss/goforjj/genapp
-//go:generate forjj-genapp github.yaml vendor/github.com/forj-oss/goforjj/genapp
